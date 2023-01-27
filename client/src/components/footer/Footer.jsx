c35a8cb37c3dbed308973aceda875d367dd7acee
Divyansh-0 <divyansh07mishra@gmail.com>
2023-01-28 00:20:04 +0530

    first commit

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="fText">Copyright ©2023 ❤️ to Travel.</div>
+    </div>
+  );
+};
+
+export default Footer;
